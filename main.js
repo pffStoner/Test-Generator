a6176367f98d51de7e523bcd94045c368cf1d3cc
Dimitar Kokotanekov <acid901@mail.bg>
2018-05-07 00:00:25 +0300

    display and create question functions

--- main.js
+++ main.js
@@ -1,16 +1,68 @@
 
+function getFormInfo() {
+    let x = document.getElementById("form");
+    let txt = "";
+    let formRes = [];
+    
+    for (let i = 0; i < x.length; i++) {
+       // txt = txt + x.elements[i].value + "<br>";
+       formRes.push(x[i].value);
+      // console.log(x[i].value);
+    }  
+    console.log(formRes.slice(1,3));
+    console.log(formRes);
+    return formRes;
+}
 
+let Questions = function (question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+Questions.prototype.displayQuestion = function(){
+    let qStr = "<div class='question'>"+this.question;
+    //let answers = getFormInfo.slice(1,4);
+   // $('.col-md-10').append("<div class='question'>"+this.question);
+    for (let i = 0; i < this.answers.length; i++) {
+        qStr+= "<div class='form-check'>"+
+        "<input class='form-check-input' type='radio' name='exampleRadios' id='exampleRadios1' value='option1' checked>"+
+        "<label class='form-check-label' for='exampleRadios1'>"+
+         this.answers[i]+"</label>"+"</div>"    
+    }
+    qStr += "</div>";
+    $('.col-md-10').append(qStr);
+}
+
+function createQuestion() {
+   let form =  getFormInfo();
+    let question = new Questions(form[0], form.slice(1,5), form[5]);
+    question.displayQuestion();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let question = prompt('Въпрос');
 
 let a = prompt('Отг А');
-/*
+
 let b = prompt('Отг Б');
 let c = prompt('Отг В');
 let d = prompt('Отг Г');
 
 */
 
-
+/*
 $('.col-md-10').append("<div class='question'>"
                                         +question+
                                         "<div class='form-check'>"+
@@ -27,34 +79,9 @@ $('.col-md-10').append("<div class='question'>"
                                            a+"</label>"+"</div>"+
                                         "</div>");
 
-function getFormInfo() {
-    let x = document.getElementById("form");
-    let txt = "";
-    let formRes = [];
-    
-    for (let i = 0; i < x.length; i++) {
-       // txt = txt + x.elements[i].value + "<br>";
-       formRes.push(x[i].value);
-       console.log(x[i].value);
+                                        
 
-
-    }
-    
-    return formRes;
-}
-
-let form = getFormInfo();
-console.log(form);
-
-
-
-
-
-
-
-
-
-
+                                        */
 
 
 
